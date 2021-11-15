b00386e395363d60b0408bc19622e0cf65923a09
Florian Dugué <79923290+Flow78300@users.noreply.github.com>
2021-11-15 10:20:55 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$characters = [
+
+    "Florian" => [
+
+        "city" => "Paname",
+
+        "weapon" => "V4 Power"
+
+    ],
+
+    "Negan" => [
+
+        "city" => "The Sanctuary",
+
+        "weapon" => "Lucille"
+
+    ],
+
+    "Daryl" => [
+
+        "city" => "Alexandria",
+
+        "weapon" => "crossbow"
+
+    ],
+
+    "Ezekiel" => [
+
+        "city" => "The Kingdom",
+
+        "weapon" => "Shiva"
+
+    ]
+
+];
+
+var_dump($characters);
+
+
+?>
